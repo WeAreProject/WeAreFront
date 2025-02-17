2c7670a82f05798da9543114dd0840b8156784fd
anora42 <johansinoe2049@gmail.com>
2025-02-17 14:33:15 -0600

    tailwindcss

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../index.css'
+
+const Home : React.FC = () => {
+  return (
+    <h1  className="text-4x1 font-bold "> Hola mundo</h1>
+  )
+};
+
+export default Home;
